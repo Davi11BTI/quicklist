741bd193877fad55142d32ae4702e3bb3e910f80
DavI Lindão <davievangelistasampaio@gmail.com>
2025-06-09 02:01:21 +0000

    criação do arquivo main.js e criação da função addItem para adicionar itens no array

---
+++ main.js
@@ -0,0 +1,14 @@
+const itens = []
+
+function addItem(){
+    const itemName = document.querySelector("#Item").value
+
+    const item = {
+        name : itemName,
+        checked : false
+    }
+
+    itens.push(item)
+
+    document.querySelector("#item").value = ""
+}
